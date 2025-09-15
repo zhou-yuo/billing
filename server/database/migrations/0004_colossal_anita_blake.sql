e0fb33ec8f95a791650140d1b62f5dd338da3e30
zhou-yu <zhouyuo995@gmail.com>
2025-09-15 14:41:23 +0700

    init

---
+++ server/database/migrations/0004_colossal_anita_blake.sql
@@ -0,0 +1,21 @@
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `__new_transactions` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`description` text NOT NULL,
+	`amount` real NOT NULL,
+	`transaction_date` integer NOT NULL,
+	`payer_id` text NOT NULL,
+	`participants` text NOT NULL,
+	`type` text NOT NULL,
+	`borrower_id` text,
+	`lender_id` text,
+	`created_at` integer DEFAULT '"2025-09-15T07:38:06.633Z"' NOT NULL,
+	FOREIGN KEY (`payer_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`borrower_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`lender_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+INSERT INTO `__new_transactions`("id", "description", "amount", "transaction_date", "payer_id", "participants", "type", "borrower_id", "lender_id", "created_at") SELECT "id", "description", "amount", "transaction_date", "payer_id", "participants", "type", "borrower_id", "lender_id", "created_at" FROM `transactions`;--> statement-breakpoint
+DROP TABLE `transactions`;--> statement-breakpoint
+ALTER TABLE `__new_transactions` RENAME TO `transactions`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;
